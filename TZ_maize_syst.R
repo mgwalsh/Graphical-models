ded4da4794b24debf9f2fef03e367087d786079e
mgwalsh <markusgwalsh@gmail.com>
2017-03-09 09:24:20 +0300

    log-linear interaction models of Tanzania MobileSurvey "maize system" data, 2016

---
+++ TZ_maize_syst.R
@@ -0,0 +1,27 @@
+#' log-linear interaction models of Tanzania MobileSurvey "maize system" data, 2016
+#' M. Walsh, March 2017
+ 
+# install.packages(c("downloader","gRim","statnet"), dependencies=T)
+suppressPackageStartupMessages({
+  require(downloader)
+  require(gRim)
+  require(statnet)
+})
+
+# Data setup --------------------------------------------------------------
+# Create a data folder in your current working directory
+dir.create("MS_data", showWarnings=F)
+setwd("./MS_data")
+
+# Download 
+download("https://www.dropbox.com/s/3sx5m2cu4cyi37g/maize_system.csv?raw=1", "maize_system.csv", mode="wb")
+maize <- read.table("maize_system.csv", header=T, sep=",")
+maize <- maize[,4:11]
+dim(maize)
+
+gm1 <- dmod(~.^., data = maize)
+gm2 <- stepwise(dm1, criterion = "test", alpha=0.0001)
+
+dag <- ugList(terms(gm2), result="matrix")
+net <- as.network(x = dag, directed = FALSE, loops = FALSE, matrix.type = "adjacency")
+plot.network(net, vertex.col = "white", vertex.cex = 8, displaylabels = T, label.pos = 5, mode = "circle")
